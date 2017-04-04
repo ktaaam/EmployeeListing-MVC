557f889880bb96f732c0ac7e4303f7f8e7d6a787
ktaaam <kevint4m@gmail.com>
2017-04-04 04:37:25 -0700

    Added EmployeeMVC into Github

---
+++ EmployeeMVC/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string employee_Name { get; set; }
+        public string job_title { get; set; }
+        public DateTime date_started { get; set; }
+        public DateTime date_Ended { get; set; }
+        public string image { get; set; }
+        public byte[] imgUpload { get; set; }
+    }
+
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext() : base("DefaultConnection") { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
