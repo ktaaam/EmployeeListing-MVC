ff2195aeb6883e92284c01deb5f3e42fe9161953
ktaaam <kevint4m@gmail.com>
2017-04-04 04:31:01 -0700

    EmployeeListing using MVC

---
+++ MVCEmployeeList/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcEmployeeList.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string employee_Name { get; set; }
+        public string job_title { get; set; }
+        public DateTime date_started { get; set; }
+        public DateTime date_Ended { get; set; }
+        public byte[] image { get; set; }
+    }
+
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext() : base("DefaultConnection") { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MVCEmployeeList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCEmployeeList.Startup))]
+namespace MVCEmployeeList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
